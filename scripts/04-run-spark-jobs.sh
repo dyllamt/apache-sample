5e950a0c317940f3f7f25816a3577fca8707b99f
dyllamt <maxwell.dylla@gmail.com>
2023-12-04 20:41:22 -0800

    update timeout for spark-jobs

--- scripts/04-run-spark-jobs.sh
+++ scripts/04-run-spark-jobs.sh
@@ -20,6 +20,7 @@ while : ; do
         exit 1
     fi
     if [[ $elapsed -ge $TIMEOUT ]]; then
+        kubectl get sparkapplication $APP_NAME -n $NAMESPACE
         echo "Timed out waiting for the application to start."
         exit 1
     fi
@@ -51,6 +52,7 @@ while : ; do
         exit 1
     fi
     if [[ $elapsed -ge $TIMEOUT ]]; then
+        kubectl get sparkapplication $APP_NAME -n $NAMESPACE
         echo "Timed out waiting for the application to finish."
         exit 1
     fi
